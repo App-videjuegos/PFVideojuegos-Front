1c4cce21a95ad484b03d0723886fd284f1631ca4
gustavosprx <gustavo.sprx@gmail.com>
2023-07-14 01:07:28 -0300

    Infinite Scroll

--- ecommerce-videoGameApp/src/components/views/Home/VideoGames.jsx
+++ ecommerce-videoGameApp/src/components/views/Home/VideoGames.jsx
@@ -1,114 +1,54 @@
-import { View, Text,TouchableOpacity,StyleSheet, ScrollView } from 'react-native'
+import { View, Text,TouchableOpacity,StyleSheet, FlatList } from 'react-native'
 
-import { useEffect } from 'react'
-import {useDispatch, useSelector} from "react-redux"
+ import { useEffect,useState  } from 'react'
+ import {useDispatch, useSelector} from "react-redux"
 
-import {getvideoGames } from "../../../redux/videogamesActions"
+ import {getvideoGames } from "../../../redux/videogamesActions"
 
-//Importamos componentes
-import Card from '../../utils/Card/Card'
+ //Importamos componentes
+ import Card from '../../utils/Card/Card'
 
 
-const videogames=[{
-  "id": 3498,
-  "name": "Grand Theft Auto V",
-  "descripcion": "",
-  "platforms": [
-    "PlayStation 5",
-    "Xbox Series S/X",
-    "PlayStation 4",
-    "PC",
-    "PlayStation 3",
-    "Xbox 360",
-    "Xbox One"
-  ],
-  "image": "https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg",
-  "released": "2013-09-17",
-  "rating": 4.47,
-  "genre": [
-    "Action",
-    "Adventure"
-  ],
-  "screenShots": [
-    "https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg",
-    "https://media.rawg.io/media/screenshots/a7c/a7c43871a54bed6573a6a429451564ef.jpg",
-    "https://media.rawg.io/media/screenshots/cf4/cf4367daf6a1e33684bf19adb02d16d6.jpg",
-    "https://media.rawg.io/media/screenshots/f95/f9518b1d99210c0cae21fc09e95b4e31.jpg",
-    "https://media.rawg.io/media/screenshots/a5c/a5c95ea539c87d5f538763e16e18fb99.jpg",
-    "https://media.rawg.io/media/screenshots/a7e/a7e990bc574f4d34e03b5926361d1ee7.jpg",
-    "https://media.rawg.io/media/screenshots/592/592e2501d8734b802b2a34fee2df59fa.jpg"
-  ],
-  "tiendas": [
-    "PlayStation Store",
-    "Epic Games",
-    "Steam",
-    "Xbox 360 Store",
-    "Xbox Store"
-  ],
-  "etiquetas": [
-    "Singleplayer",
-    "Steam Achievements",
-    "Multiplayer",
-    "Full controller support",
-    "Atmospheric",
-    "Great Soundtrack",
-    "RPG",
-    "Co-op",
-    "Open World",
-    "cooperative",
-    "First-Person",
-    "Third Person",
-    "Funny",
-    "Sandbox",
-    "Comedy",
-    "Third-Person Shooter",
-    "Moddable",
-    "Crime",
-    "vr mod"
-  ],
-  "create": false
-}]
+
 
 const VideoGames = ({ navigation, route }) => {
-
-  const dispatch= useDispatch();
-  const videoGames = useSelector((state) => state.videogamesState.videoGames);
-  const filteredVideoGames = useSelector((state) => state.videogamesState.filteredVideoGames);
+  const dispatch = useDispatch();
+  const vGames = useSelector((state) => state.videogamesState.videoGames);
 
 
-  useEffect(()=>{
+  useEffect(() => {
+    dispatch(getvideoGames());
+  }, []);
 
-    dispatch(getvideoGames()) ;
 
-  },[])
+
+  const renderItem = ({ item }) => (
+    <Card
+      key={item.id}
+      videoG={item}
+      nav={navigation}
+    />
+  );
+
   return (
     <View style={styles.container}>
-      <ScrollView>
-      <View>
-      {filteredVideoGames.length > 0
-            ? filteredVideoGames.map((videoGame) => (
-                <Card key={videoGame.id} videoG={videoGame} nav={navigation} />
-              ))
-            : videoGames.map((videoGame) => (
-                <Card key={videoGame.id} videoG={videoGame} nav={navigation} />
-              ))}
-  </View>
-  </ScrollView>
+      <FlatList
+        data={vGames}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id.toString()}
+
+      />
       
-      {/* <TouchableOpacity onPress={() =>navigation.navigate('Detail', {props: videogames[0]  })}> 
-                <Text style={styles.enlace2} >Enlace a ScreenDetalle</Text>
-        </TouchableOpacity>
-*/}
-    </View> 
-  )
-}
+    </View>
+  );
+};
+
 const styles = StyleSheet.create({
   container: {
-   
-    backgroundColor:'white',
-  
-  
+    flex: 1,
+    backgroundColor: 'white',
   },
+
   enlace2: {
     justifyContent: 'space-between',
     // backgroundColor: color_blanco,
@@ -117,5 +57,6 @@ const styles = StyleSheet.create({
       
   
   },
-})
-export default VideoGames
+});
+
+export default VideoGames;
