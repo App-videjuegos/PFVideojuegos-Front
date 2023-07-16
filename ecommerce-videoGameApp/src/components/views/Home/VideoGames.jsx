c1d2928fa4323502ec6713753b98a759c20a1bc2
Leandro Brangi <leandro.brangi@gmail.com>
2023-07-16 01:30:04 -0300

    Reparando home

--- ecommerce-videoGameApp/src/components/views/Home/VideoGames.jsx
+++ ecommerce-videoGameApp/src/components/views/Home/VideoGames.jsx
@@ -1,57 +1,51 @@
-import { View, Text,TouchableOpacity,StyleSheet, ScrollView } from 'react-native'
-import { useEffect } from 'react'
-import {useDispatch, useSelector} from "react-redux"
-import {getvideoGames } from "../../../redux/videogamesActions"
-//Importamos componentes
-import Card from '../../utils/Card/Card'
+import React, { useEffect } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, ScrollView, FlatList } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { getvideoGames } from '../../../redux/videogamesActions';
+
+import Card from '../../utils/Card/Card';
 
 const VideoGames = ({ navigation, route }) => {
-
-  const dispatch= useDispatch();
+  const dispatch = useDispatch();
   const videoGames = useSelector((state) => state.videogamesState.videoGames);
   const filteredVideoGames = useSelector((state) => state.videogamesState.filteredVideoGames);
 
+  useEffect(() => {
+    dispatch(getvideoGames());
+  }, []);
 
-  useEffect(()=>{
+  // Función para cargar más videojuegos
+  const fetchMoreVideoGames = () => {
+    // implementar la lógica para obtener más videojuegos desde API o fuente de datos
+  };
 
-    dispatch(getvideoGames()) ;
-
-  },[])
   return (
     <View style={styles.container}>
-      <ScrollView>
-      <View>
-      {filteredVideoGames.length > 0
-            ? filteredVideoGames.map((videoGame) => (
-                <Card key={videoGame.id} videoG={videoGame} nav={navigation} />
-              ))
-            : videoGames.map((videoGame) => (
-                <Card key={videoGame.id} videoG={videoGame} nav={navigation} />
-              ))}
-  </View>
-  </ScrollView>
+      <FlatList
+        data={filteredVideoGames.length > 0 ? filteredVideoGames : videoGames}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => <Card key={item.id} videoG={item} nav={navigation} />}
+        onEndReached={fetchMoreVideoGames} // Función a llamar cuando se alcanza el final de la lista
+        onEndReachedThreshold={0.1} // Umbral para activar la carga de más videojuegos (valor entre 0 y 1)
+        ListFooterComponent={<Text>Loading...</Text>} // Componente de carga mientras se obtienen más videojuegos
+      />
 
       {/* <TouchableOpacity onPress={() =>navigation.navigate('Detail', {props: videogames[0]  })}> 
-                <Text style={styles.enlace2} >Enlace a ScreenDetalle</Text>
-        </TouchableOpacity>
-*/}
-    </View> 
-  )
-}
+          <Text style={styles.enlace2}>Enlace a ScreenDetalle</Text>
+        </TouchableOpacity> */}
+    </View>
+  );
+};
+
 const styles = StyleSheet.create({
   container: {
-
-    backgroundColor:'white',
-
-
+    backgroundColor: 'white',
   },
   enlace2: {
     justifyContent: 'space-between',
-    // backgroundColor: color_blanco,
     alignItems: 'center',
-      fontSize:25,
-
-
+    fontSize: 25,
   },
-})
-export default VideoGames
+});
+
+export default VideoGames;
