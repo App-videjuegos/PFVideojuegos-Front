38fad0e76646c7c5311a2a3213fe10fd7e9933b7
Leandro Brangi <leandro.brangi@gmail.com>
2023-07-16 01:12:39 -0300

    Filtros reparados

--- ecommerce-videoGameApp/src/components/views/Home/VideoGames.jsx
+++ ecommerce-videoGameApp/src/components/views/Home/VideoGames.jsx
@@ -1,63 +1,57 @@
-import { View, Text,TouchableOpacity,StyleSheet, FlatList } from 'react-native'
-
- import { useEffect,useState  } from 'react'
- import {useDispatch, useSelector} from "react-redux"
-
- import {getvideoGames } from "../../../redux/videogamesActions"
-
- //Importamos componentes
- import Card from '../../utils/Card/Card'
-
-
-
+import { View, Text,TouchableOpacity,StyleSheet, ScrollView } from 'react-native'
+import { useEffect } from 'react'
+import {useDispatch, useSelector} from "react-redux"
+import {getvideoGames } from "../../../redux/videogamesActions"
+//Importamos componentes
+import Card from '../../utils/Card/Card'
 
 const VideoGames = ({ navigation, route }) => {
-  const dispatch = useDispatch();
-  const vGames = useSelector((state) => state.videogamesState.videoGames);
+
+  const dispatch= useDispatch();
+  const videoGames = useSelector((state) => state.videogamesState.videoGames);
+  const filteredVideoGames = useSelector((state) => state.videogamesState.filteredVideoGames);
 
 
-  useEffect(() => {
-    
-    dispatch(getvideoGames());
-  }, []);
+  useEffect(()=>{
 
+    dispatch(getvideoGames()) ;
 
-
-  const renderItem = ({ item }) => (
-    <Card
-      key={item.id}
-      videoG={item}
-      nav={navigation}
-    />
-  );
-
+  },[])
   return (
     <View style={styles.container}>
-      <FlatList
-        data={vGames}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id.toString()}
+      <ScrollView>
+      <View>
+      {filteredVideoGames.length > 0
+            ? filteredVideoGames.map((videoGame) => (
+                <Card key={videoGame.id} videoG={videoGame} nav={navigation} />
+              ))
+            : videoGames.map((videoGame) => (
+                <Card key={videoGame.id} videoG={videoGame} nav={navigation} />
+              ))}
+  </View>
+  </ScrollView>
 
-      />
-      
-    </View>
-  );
-};
-
+      {/* <TouchableOpacity onPress={() =>navigation.navigate('Detail', {props: videogames[0]  })}> 
+                <Text style={styles.enlace2} >Enlace a ScreenDetalle</Text>
+        </TouchableOpacity>
+*/}
+    </View> 
+  )
+}
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: 'white',
-  },
 
+    backgroundColor:'white',
+
+
+  },
   enlace2: {
     justifyContent: 'space-between',
     // backgroundColor: color_blanco,
     alignItems: 'center',
       fontSize:25,
-      
-  
-  },
-});
 
-export default VideoGames;
+
+  },
+})
+export default VideoGames
